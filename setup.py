e2c09d093f1402f5e3e02f18eeac3fa9c5e5f540
Omshinde01 <om.c.shinde01@gmail.com>
2026-04-18 14:56:57 +0530

    loggin and exception

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str) -> List[str]:
     return requirements
 
 setup(
-    name ='mlproject',
+    name ='mlproject', 
     version = '0.0.1',
     author='Om Shinde',
     author_email='om.c.shinde01@gmail.com',
